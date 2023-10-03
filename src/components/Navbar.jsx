52f05cb40b32b33060b23f7c19b0a62db65f0fc9
Kesaobaka Cassie Setlhatloso <78866517+CassMarkG@users.noreply.github.com>
2023-10-03 20:01:38 +0200

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import menu from "../assets/menu_FILL0_wght400_GRAD0_opsz24.svg";
 import '../App.css'
 
 function Navbar(){
     const [isMenuClicked, setIsMenuClicked] = useState(false);
+    const [screeWidth,setScreenWidth] = useState(window.innerWidth);
 
     //toggle menu change
     const updateMenu = () => {
       setIsMenuClicked(!isMenuClicked);
     }
+
+    useEffect(() => {
+      const changeWidth = () => {
+        setScreenWidth(window.innerWidth);
+      }
+
+      window.addEventListener('resize', changeWidth)
+
+      return () => {
+        window.removeEventListener('resize', changeWidth)
+      }
+    },[])
 
     return(
         <>
@@ -16,7 +29,7 @@ function Navbar(){
         <div className="logo">
           <h1>Logo</h1>
         </div>
-            {isMenuClicked && (
+            {(isMenuClicked || screeWidth > 810) && (
               <ul className="items" >
               <li><a className='active' href="">Home</a></li>
               <li><a href="">About</a></li>
